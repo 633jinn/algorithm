1fd21fec4f682070ebd303c4d3e83436de9f938c
633jinn <judyg00dy68@gmail.com>
2024-02-29 11:32:43 +0900

    2024.02.29 합분해 문제 해결

--- 2225_합분해.py
+++ 2225_합분해.py
@@ -1,12 +1,14 @@
 import sys
+
 input = sys.stdin.readline
 
-N,K = map(int,input().split())
+N, K = map(int, input().split())
 MOD = 1000000000
-arr = [0 for i in range(N+1)]
-for i in range(N):
-    x = 10
-    if N>9:
-        x = 
-    for j in range(10):
-        arr[i] = arr[i-j]
+# arr = [0 for i in range(N+1)]
+lst = [[1 for _ in range(N + 1)] for _ in range(K + 1)]
+if K > 1:
+    for i in range(2, K + 1):  # 정수의 갯수
+        for j in range(1, N + 1):
+            lst[i][j] = sum(lst[i - 1][0:j + 1]) % MOD
+
+print(lst[K][N] % MOD)
