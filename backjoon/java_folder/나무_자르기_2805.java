4047ffe94c4b03bc8f1d2911177ce3a8101e3428
633jinn <judyg00dy68@gmail.com>
2024-10-19 21:06:06 +0900

    20241019

---
+++ backjoon/java_folder/나무_자르기_2805.java
@@ -0,0 +1,22 @@
+package java_folder;
+
+import java.io.*;
+import java.util.*;
+
+public class 나무_자르기 {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
