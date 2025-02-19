c2dadbf7c05ebaa59733679fe1d0d62154550889
강희진 <113979494+633jinn@users.noreply.github.com>
2025-02-20 00:01:34 +0900

    [level 2] Title: 특정 물고기를 잡은 총 수 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
@@ -0,0 +1,6 @@
+select count(*) as FISH_COUNT
+    from FISH_INFO f join FISH_NAME_INFO fn
+        on f.FISH_TYPE = fn.FISH_TYPE
+    where fn.FISH_NAME = 'BASS' or fn.FISH_NAME = 'SNAPPER'
+    
+    
