62c5b5a57057f8bbb564cd412d6769a4851346aa
강희진 <113979494+633jinn@users.noreply.github.com>
2025-07-02 00:08:17 +0900

    [Gold IV] Title: 알고스팟, Time: 176 ms, Memory: 16884 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -2,96 +2,76 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    /**
-     * 다익스트라를 이용한 알고리즘
-     */
+    static int[][] maze;
+    static boolean[][] visited;
+    static PriorityQueue<Move> pq = new PriorityQueue<>((o1, o2) -> {
+        return o1.wall - o2.wall;
+    });
+    static int n, m;
+    static int[] dx = { 0, 0, 1, -1 };
+    static int[] dy = { 1, -1, 0, 0 };
 
-    static int M, N;
-    static int[][] dist;
-    static int[][] graph;
-    static PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
-    static int[] moveX = { 0, 0, -1, 1 };// 상하좌우
-    static int[] moveY = { 1, -1, 0, 0 };
-
-    public static class Node implements Comparable<Node> {
+    static class Move {
         int x;
         int y;
-        int cost;
+        int wall;
 
-        public Node(int x, int y, int cost) {
+        public Move(int x, int y, int wall) {
             this.x = x;
             this.y = y;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Node node) {
-            return this.cost - node.cost;
+            this.wall = wall;
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-
-        dist = new int[N + 1][M + 1];
-        graph = new int[N + 1][M + 1];
-        for (int i = 1; i < N + 1; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        for (int i = 1; i < N + 1; i++) {
-            String[] str = br.readLine().split("");
-            for (int j = 1; j < M + 1; j++) {
-                if (str[j - 1].equals("1")) {
-                    graph[i][j] = 1;
-                }
+    public static void main(String[] args) throws Exception {
+        input();
+        pq.offer(new Move(1, 1, 0));
+        visited[1][1] = true;
+        while (!pq.isEmpty()) {
+            Move move = pq.poll();
+            int x = move.x;
+            int y = move.y;
+            int wall = move.wall;
+            if (x == n && y == m) {
+                System.out.println(wall);
+                return;
             }
-        }
-
-        priorityQueue.add(new Node(1, 1, 0));
-        if (!(N == 1 && M == 1)) {
-            dijkstra();
-            bw.write(dist[N][M] + "\n");
-        }else{
-            bw.write(0 + "\n");
-        }
-
-        bw.flush();
-        bw.close();
-    }
-
-    private static void dijkstra() {
-        while (!priorityQueue.isEmpty()) {
-            Node now = priorityQueue.poll();
             for (int i = 0; i < 4; i++) {
-                int x = now.x + moveX[i];
-                int y = now.y + moveY[i];
-                if (checkPosition(x, y)) {
-                    continue;
-                }
-                if (graph[x][y] == 0 && dist[x][y] > now.cost) { // 빈방일 때,
-                    dist[x][y] = now.cost;
-                    priorityQueue.offer(new Node(x, y, now.cost));
-                } else if (graph[x][y] == 1 && dist[x][y] > now.cost + 1) {// 벽을 뚫어야할 떄,
-                    dist[x][y] = now.cost + 1;
-                    priorityQueue.offer(new Node(x, y, now.cost + 1));
+                int moveX = x + dx[i];
+                int moveY = y + dy[i];
+                if (isValid(moveX, moveY) && !visited[moveX][moveY]) {
+                    visited[moveX][moveY] = true;
+                    if (maze[moveX][moveY] == 1)
+                        pq.offer(new Move(moveX, moveY, wall + 1));
+                    else
+                        pq.offer(new Move(moveX, moveY, wall));
                 }
             }
         }
     }
 
-    // 맵 밖으로 나갔는지 확인
-    private static boolean checkPosition(int x, int y) {
-        if (x < 1 || x > N) {
-            return true;
+    private static boolean isValid(int moveX, int moveY) {
+        if (moveX > n || moveX < 1) {
+            return false;
         }
-        if (y < 1 || y > M) {
-            return true;
+        if (moveY > m || moveY < 1) {
+            return false;
         }
-        return false;
+        return true;
+    }
+
+    private static void input() throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        maze = new int[n + 1][m + 1];
+        visited = new boolean[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            String[] string = br.readLine().split("");
+            for (int j = 0; j < m; j++) {
+                maze[i][j + 1] = Integer.parseInt(string[j]);
+            }
+        }
     }
 }
